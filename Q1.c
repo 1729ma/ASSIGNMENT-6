dfc3efb7a937f89640e195f24b61b17b5b262df7
1729ma <166897218+1729ma@users.noreply.github.com>
2025-01-15 14:59:01 +0530

    Create Q1.c

---
+++ Q1.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+void reverseArray(int arr[], int size) {
+    int start = 0;
+    int end = size - 1;
+
+    while (start < end) {
+        // Swap the elements at start and end
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        // Move the indices towards the center
+        start++;
+        end--;
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int size;
+
+    // Take the size of the array as input
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int arr[size]; // Declare an array of the given size
+
+    // Take array elements as input
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Original array: ");
+    printArray(arr, size);
+
+    reverseArray(arr, size);
+
+    printf("Reversed array: ");
+    printArray(arr, size);
+
+    return 0;
+}
