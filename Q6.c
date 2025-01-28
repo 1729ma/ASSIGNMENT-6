6d5bb87213535211d9ca8877466c0132aac9a2f8
1729ma <166897218+1729ma@users.noreply.github.com>
2025-01-28 21:43:20 +0530

    Create Q6.c

---
+++ Q6.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int marks[5][5], total[5] = {0}, i, j;
+    int hm = 0, hmi = 0; //declaring the varaibles
+
+    // Accepting marks from the user 
+    for(i = 0; i < 5; i++) {
+        printf("Enter marks for Student %d:\n", i + 1);
+        for(j = 0; j < 5; j++) {
+            printf("Subject %d: ", j + 1);
+            scanf("%d", &marks[i][j]);
+            total[i] += marks[i][j];  // Calculate total marks for each student
+        }
+    }
+
+    // Printing the total marks obtained by all students
+    printf("\nTotal marks obtained by each student:\n");
+    for(i = 0; i < 5; i++) {
+        printf("Student %d: %d\n", i + 1, total[i]);
+
+        // Determine the highest total marks
+        if(total[i] > hm) {
+            hm = total[i];
+            hmi = i;
+        }
+    }
+
+    // Printing the highest total marks
+    printf("\nHighest total marks obtained: %d by Student %d\n", hm, hmi + 1);
+
+    return 0;
+}
